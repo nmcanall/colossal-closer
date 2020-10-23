0b9f6aa97bd4411055d2bad71304e732f7a154c3
chulopapi <marlon.guandique@gmail.com>
2020-10-22 19:58:28 -0700

    fixing initial pull for start/point

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
-import Footer from './components/Footer';
+import Footer from './Footer';
 import Dashboard from './components/dashboard';
 import Customers from './components/customers';
 import Sales from './components/sales';
