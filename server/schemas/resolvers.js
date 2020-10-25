936109b50d34b03593e6ec0f22a02b42499f89a4
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-25 12:05:58 -0700

    catch up

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,31 @@
 const { AuthenticationError } = require('apollo-server-express')
 const { signToken } = require('../utils/auth')
 
+function checkLoggedIn(user) {
+    if (!user) {
+        throw new AuthenticationError("You're not logged in")
+    }
+}
 const resolvers = {
     Query: {
+        employee: async (parent, { _id }) => {
+
+        },
+        customer: async (parent, { _id }) => {
+
+        },
+        customers: async () => {
+
+        }
 
     },
     Mutation: {
-
+        // addEmployee(firstName: String!, lastName: String!, email: String!, password: String!, customers:[Customer]): Auth
+        // updateEmployee(firstName: String, lastName: String, email: String, password: String, customer:[Customer]): Employee
+        // login(email: Sring!, password: String!): Auth
+        // addCustomer(input: AddCustomerInput!): Customer
+        // updateCustomer(input: UpdateCustomerInput!): Customer
+        
     }
 }
 
