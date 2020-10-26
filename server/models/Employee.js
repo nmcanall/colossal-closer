857a0172d4c842d2f7222296d49cccac7b48ab7d
nmcanall <nmcanall@citadel.edu>
2020-10-25 20:34:23 -0700

    add specific customer counts and total dollars sold to an employee

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -3,58 +3,100 @@ const bcrypt = require('bcrypt');
 const moment = require("moment");
 
 const employeeSchema = new Schema(
-    {
-        firstName: {
-            type: String,
-            required: true,
-            trim: true
-        },
-
-        lastName: {
-            type: String,
-            required: true,
-            trim: true
-        },
-
-        // Email must be unique and use colossalcloser.com
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: [/.+@colossalcloser\.com/, 'Must use a valid Colossal Closer email address!']
-        },
-
-        // Password is created by the employee and hashed for storage
-        password: {
-            type: String,
-            required: true,
-            minlength: 5
-        },
-
-        // Created at shows when the employee joined the company, and therefore, employee's tenure
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: timestamp => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a')
-        },
-
-        // Array of all customers defined in the Customer model
-        customers: [{
-            type: Schema.Types.ObjectId,
-            ref: "Customer"
-        }]
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true
     },
-    {
-      toJSON: {
-        virtuals: true
-      }
+
+    lastName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    // Email must be unique and use colossalcloser.com
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@colossalcloser\.com/, 'Must use a valid Colossal Closer email address!']
+    },
+
+    // Password is created by the employee and hashed for storage
+    password: {
+      type: String,
+      required: true,
+      minlength: 5
+    },
+
+    // Created at shows when the employee joined the company, and therefore, employee's tenure
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a')
+    },
+
+    // Array of all customers defined in the Customer model
+    customers: [{
+      type: Schema.Types.ObjectId,
+      ref: "Customer"
+    }]
+  },
+  {
+    toJSON: {
+      virtuals: true
     }
+  }
 );
 
 // Counts the number of customers from an employee has/is working with
-// More detail will be needed for more precise counts for employee/customer relations (won, lost, active, etc)
 employeeSchema.virtual("customerCount").get(function() {
-    return this.customers.length;
+  return this.customers.length;
+});
+
+// Counts the number of active customers for a given employee
+employeeSchema.virtual("activeCustomerCount").get(function() {
+  let count = 0;
+  for(let i = 0; i < this.customers.length; i++) {
+    if(this.customers[i].status === "active") {
+      count++;
+    }
+  }
+  return count;
+});
+
+// Counts the number of won customers for a given employee
+employeeSchema.virtual("wonCustomerCount").get(function() {
+  let count = 0;
+  for(let i = 0; i < this.customers.length; i++) {
+    if(this.customers[i].status === "won") {
+      count++;
+    }
+  }
+  return count;
+});
+
+// Counts the number of lost customers for a given employee
+employeeSchema.virtual("lostCustomerCount").get(function() {
+  let count = 0;
+  for(let i = 0; i < this.customers.length; i++) {
+    if(this.customers[i].status === "lost") {
+      count++;
+    }
+  }
+  return count;
+});
+
+// Counts the total dollars sold by a given employee
+employeeSchema.virtual("dollarsSold").get(function() {
+  let dollars = 0;
+  // Loop through each customer for the dollars sold value
+  for(let i = 0; i < this.customers.length; i++) {
+    dollars += this.customers[i].dollarsSold;
+  }
+  return dollars;
 });
 
 // set up pre-save middleware to create password
