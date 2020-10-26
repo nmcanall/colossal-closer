26c82911d664b12be2173af3ce4936a10ae018f0
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-26 16:38:15 -0700

    set up queries, began mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,77 @@
+import gql from 'graphql-tag'
+
+export const QUERY_EMPLOYEES = gql`
+    {
+        employees {
+            firstName
+            lastName
+            customerCount
+            activeCustomerCount
+            wonCustomerCount
+            lostCustomerCount
+            dollarsSold
+            customers {
+                _id
+            }
+        }
+    }
+`
+export const QUERY_EMPLOYEE = gql`
+    query getEmployee($_id: ID!) {
+        employee(_id: $_id) {
+            firstName
+            lastName
+            customerCount
+            activeCustomerCount
+            wonCustomerCount
+            lostCustomerCount
+            dollarsSold
+            customers {
+                _id
+            }
+        }
+    }
+`
+export const QUERY_CUSTOMERS = gql`
+    query getCustomers($_id: ID) {
+        customers(_id: $_id ) {
+            _id
+            businessName
+            contactName
+            phone
+            email
+            salesman {
+                _id
+            }
+            createdAt
+            status
+            dollarsSold
+            transactionsWon
+            contacts {
+                type
+                note
+            }
+        }
+    }
+`
+export const QUERY_CUSTOMER = gql`
+    query getCustomer($_id: ID) {
+        customers(_id: $_id ) {
+            businessName
+            contactName
+            phone
+            email
+            salesman {
+                _id
+            }
+            createdAt
+            status
+            dollarsSold
+            transactionsWon
+            contacts {
+                type
+                note
+            }
+        }
+    }
+`
