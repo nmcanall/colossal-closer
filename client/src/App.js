d53d0a78660fe7105e23d294c9ff15aa6ed80c6e
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-26 17:20:58 -0700

    added apolloprovider to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,44 +11,44 @@ import AllCustomers from './pages/AllCustomers';
 
 // import Contact from './components/Contact';
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import { ApolloProvider } from '@apollo/react-hooks';
-// import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
 // borrowed from the classwork, will uncomment when the JWT auth is set up
-// const client = new ApolloClient({
-//     request: (operation) => {
-//       const token = localStorage.getItem('id_token')
-//       operation.setContext({
-//         headers: {
-//           authorization: token ? `Bearer ${token}` : ''
-//         }
-//       })
-//     },
-//     uri: '/graphql',
-// })
+const client = new ApolloClient({
+    request: (operation) => {
+      const token = localStorage.getItem('id_token')
+      operation.setContext({
+        headers: {
+          authorization: token ? `Bearer ${token}` : ''
+        }
+      })
+    },
+    uri: '/graphql',
+})
 
 function App() {
     const [pages] = useState(["dashboard", "customers", "sales", "other"]);
     const [pageSelected, setPageSelected] = useState(pages[0]);
 
     return (
+        <ApolloProvider client={client}>
+            <div id="html">
+                <Header pageSelected={pageSelected}
+                    setPageSelected={setPageSelected} />
+                <main className="">
+                    {/* <Dashboard></Dashboard> */}
+                    {/* <Login></Login> */}
+                    {/* <Signup></Signup> */}
+                    <AllCustomers></AllCustomers>
 
+                </main>
+                <Footer>
+                    
+                </Footer>
 
-        <div id="html">
-            <Header pageSelected={pageSelected}
-                setPageSelected={setPageSelected} />
-            <main className="">
-                {/* <Dashboard></Dashboard> */}
-                {/* <Login></Login> */}
-                {/* <Signup></Signup> */}
-                <AllCustomers></AllCustomers>
-
-            </main>
-            <Footer>
-                
-            </Footer>
-
-        </div>
+            </div>
+        </ApolloProvider>
     );
 };
 
